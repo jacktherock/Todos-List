38f32f96ca71b32302273f01b00113fe303bd6a6
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-08-17 14:42:13 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const About = () => {
+  let myStyle = {
+    minHeight: "70vh",
+    margin: "45px auto",
+  };
+  return (
+    <div className="container" style={myStyle}>
+      <div>
+        <h1>About Us</h1>
+
+        <p>
+          <h5>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum
+            corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam
+            itaque ipsam possimus. Inventore vitae, perferendis harum quas
+            cupiditate adipisci repellendus tempore asperiores at consequatur
+            quos?
+          </h5>
+        </p>
+      </div>
+    </div>
+  );
+};
